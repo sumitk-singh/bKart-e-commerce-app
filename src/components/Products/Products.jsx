138eb77cfbbebf926dd490058f3b71fc7887c351
SUMIT SINGH <76554602+sumitk-singh@users.noreply.github.com>
2022-11-25 08:58:41 +0530

    added toast

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -5,12 +5,22 @@ import { useFilter } from "../../Contexts/filterContext";
 import Sidebar from "../Sidebar/Sidebar";
 import { Link } from "react-router-dom";
 
+import React from 'react';
+
+import {ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+
+
 const Products = () => {
 
     const { filteredProduct } = useFilter();
     const { addToWishList } = useWishList();
     const { addToCart } = useCart();
     // console.log(filteredProduct);
+
+    const notifyForCart = () => toast("Added to cart");
+    const notifyForWishlist = () => toast("Added to wishlist");
 
     return (
         <>
@@ -22,6 +32,8 @@ const Products = () => {
 
                     {
                         filteredProduct().map((product, _id) => {
+
+                            
 
                             return <div className="prod-shirt" key={_id}>
                                 <div className="vertical-card-with-shadow">
@@ -41,8 +53,9 @@ const Products = () => {
                                         </div>
                                         <div className="card-btn">
                                             <Link to="/Cart"><button className="buy-btn" onClick={() => addToCart({ _id, product })}>Buy</button></Link>
-                                            <button className="add-to-wishlist" href="#url" onClick={() => addToWishList({ product })}><i class="fas fa-heart" ></i> </button>
-                                            <button className="add-to-cart" href="#url" onClick={() => addToCart({ _id, product })}><i class="fas fa-shopping-cart"></i> </button>
+                                            <button className="add-to-wishlist" href="#url" onClick={() => {addToWishList({ product }); notifyForWishlist()}}><i class="fas fa-heart" ></i> </button>
+                                            <button className="add-to-cart" href="#url" onClick={() => {addToCart({ _id, product }); notifyForCart()}}><i class="fas fa-shopping-cart"></i> </button>
+                                            <ToastContainer />
                                         </div>
                                     </div>
                                 </div>
